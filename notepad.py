c4afb1a8637b00f20e1051ab906cdfa2ad345b0d
ZeroMarker <u78k4l67@163.com>
2023-05-31 01:21:02 +0800

    Edit

--- notepad.py
+++ notepad.py
@@ -13,75 +13,104 @@ workdir = "D:/document/lang/"
 # Set variable to access
 global openStatusName
 openStatusName = False
+global selected
+selected = False
 
 # Create file function
-def newFile():
+
+
+def new_file():
     # Delete previous text
     textBox.delete("1.0", END)
     # Update status bar
     root.title("New file - Text Pad")
     statusBar.config(text=f"New File{whitespace}")
 
-def openFile():
+
+def open_file():
     # Delete previous text
     textBox.delete("1.0", END)
 
     # Grab filename
-    textFile = filedialog.askopenfile(initialdir=workdir, title="Open File",
-                                      filetypes=(("Text Files", "*.txt"), ("HTML Files", "*.html"),
-                                                 ("All Files", "*.*")))
+    text_file = filedialog.askopenfile(initialdir=workdir, title="Open File",
+                                       filetypes=(("Text Files", "*.txt"), ("HTML Files", "*.html"),
+                                                  ("All Files", "*.*")))
     # Check file name is true
-    if textFile:
+    if text_file:
         global openStatusName
-        openStatusName = textFile
+        openStatusName = text_file
 
     # Update status bar
-    name = textFile.name
+    name = text_file.name
     statusBar.config(text=f"{name}{whitespace}")
     name = name.replace(workdir, "")
     root.title(f"{name} - Text Pad")
     # Open file
-    textFile = open(textFile.name, 'r', encoding='utf8')
-    textFile = open(textFile.name, 'r', encoding='utf8')
-    stuff = textFile.read()
+    text_file = open(text_file.name, 'r', encoding='utf8')
+    text_file = open(text_file.name, 'r', encoding='utf8')
+    stuff = text_file.read()
     # Add file to textbox
     textBox.insert(END, stuff)
     # Close the opened file
-    textFile.close()
+    text_file.close()
+
 
 # Save file
-def saveAsFile():
-    textFile = filedialog.asksaveasfilename(defaultextension=".*", initialdir=workdir,
-                                            title="Save File", filetypes=(("Text Files", "*.txt"),
-                                                                          ("HTML Files", "*.html"),
-                                                                          ("All Files", "*.*")))
-    if textFile:
+
+
+def save_as_file():
+    text_file = filedialog.asksaveasfilename(defaultextension=".*", initialdir=workdir,
+                                             title="Save File", filetypes=(("Text Files", "*.txt"),
+                                                                           ("HTML Files", "*.html"),
+                                                                           ("All Files", "*.*")))
+    if text_file:
         # Update status bar
-        name = textFile
+        name = text_file
         statusBar.config(text=f"Saved: {name}")
         name = name.replace(workdir, "")
         root.title(f"{name} - Text Pad")
 
         # Save file
-        textFile = open(textFile, 'w', encoding='utf8')
-        textFile.write(textBox.get(1.0, END))
+        text_file = open(text_file, 'w', encoding='utf8')
+        text_file.write(textBox.get(1.0, END))
         # Close file
-        textFile.close()
+        text_file.close()
 
 
-def saveFile():
+def save_file():
     global openStatusName
     name = openStatusName.name
     if name:
         # Save file
-        textFile = open(name, 'w')
-        textFile.write(textBox.get(1.0, END))
+        text_file = open(name, 'w')
+        text_file.write(textBox.get(1.0, END))
         # Close file
-        textFile.close()
+        text_file.close()
         # Set file status
         statusBar.config(text=f"Saved: {name}")
     else:
-        saveAsFile()
+        save_as_file()
+
+
+# Copy Texts
+
+
+def copy_text(e):
+    pass
+
+
+def cut_text(e):
+    if textBox.selection_get():
+        # Grab selected text
+        selected = textBox.selection_get()
+        # Delete selected text
+        textBox.delete("sel.first", "sel.last")
+
+
+def paste_text(e):
+    if selected:
+        position = textBox.index(INSERT)
+        textBox.insert(position, selected)
 
 
 # Create main frame
@@ -108,19 +137,19 @@ root.config(menu=myMenu)
 # Add file menu
 fileMenu = Menu(myMenu, tearoff=False)
 myMenu.add_cascade(label="File", menu=fileMenu)
-fileMenu.add_command(label="New", command=newFile)
-fileMenu.add_command(label="Open", command=openFile)
-fileMenu.add_command(label="Save", command=saveFile)
-fileMenu.add_command(label="Save as", command=saveAsFile)
+fileMenu.add_command(label="New", command=new_file)
+fileMenu.add_command(label="Open", command=open_file)
+fileMenu.add_command(label="Save", command=save_file)
+fileMenu.add_command(label="Save as", command=save_as_file)
 fileMenu.add_separator()
 fileMenu.add_command(label="Exit", command=root.quit)
 
 # Add edit menu
 editMenu = Menu(myMenu, tearoff=False)
 myMenu.add_cascade(label="Edit", menu=editMenu)
-editMenu.add_command(label="Copy")
-editMenu.add_command(label="Paste")
-editMenu.add_command(label="Cut")
+editMenu.add_command(label="Copy", command=lambda: copy_text(False))
+editMenu.add_command(label="Paste", command=lambda: paste_text(False))
+editMenu.add_command(label="Cut", command=lambda: cut_text(False))
 editMenu.add_command(label="Undo")
 editMenu.add_command(label="Redo")
 
